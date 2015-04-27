5aa76da529fa22f5e1a0540d3561a9d858bffd83
Michael Ritter <shake@umiacs.umd.edu>
2015-04-27 15:23:43 -0400

    breakout endpoint for EarthSettings to initialize properly

--- src/main/java/org/chronopolis/earth/EarthSettings.java
+++ src/main/java/org/chronopolis/earth/EarthSettings.java
@@ -1,47 +1,30 @@
 package org.chronopolis.earth;
 
+import org.chronopolis.earth.models.Endpoint;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by shake on 11/14/14.
  */
+@Component
 @ConfigurationProperties(prefix = "earth")
 public class EarthSettings {
 
-    List<Endpoint> endpoints;
+    List<Endpoint> endpoints = new ArrayList<>();
 
-    static class Endpoint {
-        String authKey;
-        String apiRoot;
-        String name;
-
-        public String getApiRoot() {
-            return apiRoot;
-        }
-
-        public Endpoint setApiRoot(String apiRoot) {
-            this.apiRoot = apiRoot;
-            return this;
-        }
-
-        public String getAuthKey() {
-            return authKey;
-        }
-
-        public Endpoint setAuthKey(String authKey) {
-            this.authKey = authKey;
-            return this;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Endpoint setName(String name) {
-            this.name = name;
-            return this;
-        }
+    public List<Endpoint> getEndpoints() {
+        return endpoints;
     }
+
+    /*
+    public EarthSettings setEndpoints(List<Endpoint> endpoints) {
+        this.endpoints = endpoints;
+        return this;
+    }
+    */
+
 }
