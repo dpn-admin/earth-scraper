3063d3aaadf04773004cdbd72ef408aac13fa108
Michael Ritter <shake@umiacs.umd.edu>
2015-05-01 15:22:08 -0400

    add class to support the Python RESTful serialization of abag

---
+++ src/main/java/org/chronopolis/earth/models/Fixity.java
@@ -0,0 +1,43 @@
+package org.chronopolis.earth.models;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by shake on 5/1/15.
+ */
+public class Fixity {
+
+    String algorithm;
+    String digest;
+    DateTime createdAt;
+
+    public Fixity() {
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public Fixity setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+        return this;
+    }
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public Fixity setDigest(String digest) {
+        this.digest = digest;
+        return this;
+    }
+
+    public DateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public Fixity setCreatedAt(DateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+}
