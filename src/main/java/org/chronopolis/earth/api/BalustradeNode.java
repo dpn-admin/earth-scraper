bcbb816fe5f835dc69f3be91cda6c0b258cb8f12
Michael Ritter <shake@umiacs.umd.edu>
2015-07-09 16:04:55 -0400

    add callback based methods for the api interfaces

--- src/main/java/org/chronopolis/earth/api/BalustradeNode.java
+++ src/main/java/org/chronopolis/earth/api/BalustradeNode.java
@@ -2,6 +2,7 @@ package org.chronopolis.earth.api;
 
 import org.chronopolis.earth.models.Node;
 import org.chronopolis.earth.models.Response;
+import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
 import retrofit.http.QueryMap;
@@ -18,7 +19,13 @@ public interface BalustradeNode {
     @GET("/api-v1/node/")
     Response<Node> getNodes(@QueryMap Map<String, Integer> params);
 
+    @GET("/api-v1/node/")
+    void getNodes(@QueryMap Map<String, Integer> params, Callback<Response<Node>> callback);
+
     @GET("/api-v1/node/{name}/")
     Node getNode(@Path("name") String name);
+
+    @GET("/api-v1/node/{name}/")
+    void getNode(@Path("name") String name, Callback<Node> callback);
 
 }
--- src/main/java/org/chronopolis/earth/api/BalustradeTransfers.java
+++ src/main/java/org/chronopolis/earth/api/BalustradeTransfers.java
@@ -3,6 +3,8 @@ package org.chronopolis.earth.api;
 import org.chronopolis.earth.models.Replication;
 import org.chronopolis.earth.models.Response;
 import org.chronopolis.earth.models.Restore;
+import org.springframework.beans.factory.annotation.Value;
+import retrofit.Callback;
 import retrofit.http.Body;
 import retrofit.http.GET;
 import retrofit.http.PATCH;
@@ -24,35 +26,62 @@ public interface BalustradeTransfers {
     @GET("/api-v1/replicate/")
     Response<Replication> getReplications(@QueryMap Map<String, String> params);
 
+    @GET("/api-v1/replicate/")
+    Response<Replication> getReplications(@QueryMap Map<String, String> params, Callback<Response<Replication>> callback);
+
     @POST("/api-v1/replicate/")
     void createReplication(@Body Replication replication);
+
+    @POST("/api-v1/replicate/")
+    void createReplication(@Body Replication replication, Callback<Void> callback);
 
     @GET("/api-v1/replicate/{id}/")
     Replication getReplication(@Path("id") String id);
 
+    @GET("/api-v1/replicate/{id}/")
+    Replication getReplication(@Path("id") String id, Callback<Replication> callback);
+
     @PUT("/api-v1/replicate/{id}/")
     Replication updateReplication(@Path("id") String id, @Body Replication replication);
 
+    @PUT("/api-v1/replicate/{id}/")
+    Replication updateReplication(@Path("id") String id, @Body Replication replication, Callback<Replication> callback);
+
     @PATCH("/api-v1/replicate/{id}/")
     Replication patchReplication(@Path("id") String id, @Body Replication replication);
+
+    @PATCH("/api-v1/replicate/{id}/")
+    Replication patchReplication(@Path("id") String id, @Body Replication replication, Callback<Replication> callback);
 
     // Restores
     @GET("/api-v1/restore/")
     Response<Restore> getRestores(@QueryMap Map<String, String> params);
 
+    @GET("/api-v1/restore/")
+    Response<Restore> getRestores(@QueryMap Map<String, String> params, Callback<Response<Restore>> callback);
+
     @POST("/api-v1/restore/")
     void createRestore(@Body Restore replication);
+
+    @POST("/api-v1/restore/")
+    void createRestore(@Body Restore replication, Callback<Void> callback);
 
     @GET("/api-v1/restore/{id}/")
     Restore getRestore(@Path("id") String id);
 
+    @GET("/api-v1/restore/{id}/")
+    Restore getRestore(@Path("id") String id, Callback<Restore> callback);
+
     @PUT("/api-v1/restore/{id}/")
     Restore updateRestore(@Path("id") String id, @Body Restore replication);
+
+    @PUT("/api-v1/restore/{id}/")
+    Restore updateRestore(@Path("id") String id, @Body Restore replication, Callback<Restore> callback);
 
     @PATCH("/api-v1/restore/{id}/")
     Restore patchRestore(@Path("id") String id, @Body Restore replication);
 
-
-
+    @PATCH("/api-v1/restore/{id}/")
+    Restore patchRestore(@Path("id") String id, @Body Restore replication, Callback<Restore> callback);
 
 }
