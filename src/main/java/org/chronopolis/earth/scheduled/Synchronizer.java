4e296ed60b11135a1ca55d5cef8cc159ffc569e4
Michael Ritter <shake@umiacs.umd.edu>
2015-07-13 17:28:29 -0400

    start to fill out how the requests will look for syncs

--- src/main/java/org/chronopolis/earth/scheduled/Synchronizer.java
+++ src/main/java/org/chronopolis/earth/scheduled/Synchronizer.java
@@ -7,7 +7,11 @@ import org.chronopolis.earth.api.BalustradeNode;
 import org.chronopolis.earth.api.BalustradeTransfers;
 import org.chronopolis.earth.api.NodeAPIs;
 import org.chronopolis.earth.api.TransferAPIs;
+import org.chronopolis.earth.models.Node;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -22,9 +26,13 @@ import java.util.Map;
  *
  * Created by shake on 3/31/15.
  */
-// @Component
-// @EnableScheduling
+@Component
+@Profile("sync")
+@EnableScheduling
 public class Synchronizer {
+
+    @Autowired
+    DateTimeFormatter formatter;
 
     @Autowired
     BagAPIs bagAPIs;
@@ -35,7 +43,8 @@ public class Synchronizer {
     @Autowired
     NodeAPIs nodeAPIs;
 
-    // @Scheduled(cron="0 55 * * * *")
+    // keep this disabled for the time being
+    // @Scheduled(cron="${cron.sync:0 0 0 * * *}")
     public void synchronize() {
         syncNode();
         syncBags();
@@ -43,23 +52,47 @@ public class Synchronizer {
     }
 
     private void syncTransfers() {
+        DateTime after = DateTime.now().minusWeeks(1);
         for (String node: transferAPIs.getApiMap().keySet()) {
             BalustradeTransfers api = transferAPIs.getApiMap().get(node);
-            api.getReplications(new HashMap());
-            api.getRestores(new HashMap());
+            api.getReplications(ImmutableMap.of(
+                    "admin_node", node,
+                    "after", formatter.print(after)));
+
+            // api.getRestores(new HashMap());
         }
     }
 
     private void syncBags() {
+        // Temporary placeholder for when we sync
+        // we'll want a better way to do this
+        DateTime after = DateTime.now().minusWeeks(1);
+        Map<String, BalustradeBag> apis = bagAPIs.getApiMap();
+        for (String node : apis.keySet()) {
+            BalustradeBag api = apis.get(node);
+            api.getBags(ImmutableMap.of(
+                    "admin_node", node,
+                    "after", formatter.print(after)));
+
+            // ourAPI.updateBag("uuid", bag, callback);
+        }
         Map<String, String> params = new ImmutableMap.Builder<String, String>()
                 .put("admin_node", "sample-node")
                 .build();
-        // bagAPI.getBags(params);
     }
 
+    /**
+     * Update a nodes current information based on its own record
+     *
+     */
     private void syncNode() {
-        String node = "sample-node";
-        // nodeAPI.getNode(node);
+        Map<String, BalustradeNode> apis = nodeAPIs.getApiMap();
+        for (String node : apis.keySet()) {
+            BalustradeNode api = apis.get(node);
+            api.getNode(node);
+            // ourAPI.updateNode(node);
+        }
+
     }
 
 }
