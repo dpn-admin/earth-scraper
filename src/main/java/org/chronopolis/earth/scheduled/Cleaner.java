97e7231e757ae91ef5b56cd5ef2f5d28a2227b09
Michael Ritter <shake@umiacs.umd.edu>
2015-07-14 14:52:16 -0400

    Add profile for cleaning the staging area

--- src/main/java/org/chronopolis/earth/scheduled/Cleaner.java
+++ src/main/java/org/chronopolis/earth/scheduled/Cleaner.java
@@ -9,6 +9,7 @@ import org.chronopolis.earth.util.BagVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -26,6 +27,7 @@ import java.util.Set;
  * Created by shake on 5/18/15.
  */
 @Component
+@Profile("clean")
 @EnableScheduling
 public class Cleaner {
     private final Logger log = LoggerFactory.getLogger(Cleaner.class);
