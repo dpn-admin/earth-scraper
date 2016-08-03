1b40da4dd02a46e82e6d245fe077e4d21b3bcebf
Michael Ritter <shake@umiacs.umd.edu>
2016-08-03 12:25:29 -0400

    Add ingest model/endpoints

---
+++ earth-core/src/main/java/org/chronopolis/earth/api/Events.java
@@ -0,0 +1,30 @@
+package org.chronopolis.earth.api;
+
+import org.chronopolis.earth.models.Ingest;
+import org.chronopolis.earth.models.Response;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.QueryMap;
+
+import java.util.Map;
+
+/**
+ * Events in DPN
+ *   Ingest
+ *   Fixity Check
+ *   Digests
+ *
+ * May look in to using rxjava in place of Call
+ *
+ * Created by shake on 8/3/16.
+ */
+public interface Events {
+
+    @GET("api-v2/ingest")
+    Call<Response<Ingest>> getIngests(@QueryMap Map<String, String> params); // TODO: 8/3/16 Map<IngestParams, String>
+
+    @POST("api-v2/ingest")
+    Call<Ingest> createIngest(@Body Ingest ingest);
+}
