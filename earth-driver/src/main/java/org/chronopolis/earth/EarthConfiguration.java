879c7ab019553fb4cf90f2041a9cd53a9d898bc6
Michael Ritter <shake@umiacs.umd.edu>
2016-08-12 18:17:21 -0400

    Changes for updated dependencies and new tables

--- earth-driver/src/main/java/org/chronopolis/earth/EarthConfiguration.java
+++ earth-driver/src/main/java/org/chronopolis/earth/EarthConfiguration.java
@@ -36,8 +36,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageImpl;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import javax.sql.DataSource;
 import java.lang.reflect.Type;
@@ -231,9 +231,10 @@ public class EarthConfiguration {
     @Bean
     DataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setDataSourceClassName("org.sqlite.SQLiteDataSource");
+        config.setJdbcUrl("jdbc:sqlite:/tmp/intake.sqlite3");
+        // config.setDataSourceClassName("org.sqlite.SQLiteDataSource");
         // TODO: we can make this configurable
-        config.addDataSourceProperty("databaseName", "/tmp/intake.sqlite3");
+        // config.addDataSourceProperty("databaseName", "/tmp/intake.sqlite3");
         return new HikariDataSource(config);
     }
 
@@ -247,6 +248,12 @@ public class EarthConfiguration {
     private void initTables(Sql2o sql2o) {
         try (Connection conn = sql2o.open()) {
             createIfNotExists(conn, "replication_flow", "CREATE TABLE replication_flow(replication_id string PRIMARY KEY, pushed TINYINT, received TINYINT, extracted TINYINT, validated TINYINT)");
+            createIfNotExists(conn, "sync_view", "CREATE TABLE sync_view(sync_id INTEGER PRIMARY KEY ASC, host string, status string, type string)");
+            // createIfNotExists(conn, "sync_detail", "CREATE TABLE sync_detail(detail_id INTEGER PRIMARY KEY ASC, sync INTEGER, detail text, FOREIGN KEY(sync) REFERENCES sync_view(sync_id))");
+            createIfNotExists(conn, "http_detail", "CREATE TABLE http_detail(http_id INTEGER PRIMARY KEY ASC, url string, request_body text, request_method string, response_code SMALLINT, response_body text, " +
+                    "sync INTEGER, replication string, " +
+                    "FOREIGN KEY(sync) REFERENCES sync_view(sync_id), " +
+                    "FOREIGN KEY(replication) REFERENCES replication_flow(replication_id))");
         }
     }
 
