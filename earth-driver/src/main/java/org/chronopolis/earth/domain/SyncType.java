72ac6821c9cbb58de64ee561c391614e044c9f32
Michael Ritter <shake@umiacs.umd.edu>
2016-10-06 13:09:49 -0400

    REST -> RESTORE

--- earth-driver/src/main/java/org/chronopolis/earth/domain/SyncType.java
+++ earth-driver/src/main/java/org/chronopolis/earth/domain/SyncType.java
@@ -5,5 +5,5 @@ package org.chronopolis.earth.domain;
  * Created by shake on 8/5/16.
  */
 public enum SyncType {
-    BAG, REPL, REST, NODE, INGEST, FIXITY, DIGEST
+    BAG, REPL, RESTORE, NODE, INGEST, FIXITY, DIGEST
 }
