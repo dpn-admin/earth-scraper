2a0cf32f0dfd932a57317f2a54439686d42c1a27
Michael Ritter <shake@umiacs.umd.edu>
2016-10-06 13:09:24 -0400

    Update SyncStatus to have generic FAIL/WARN

--- earth-driver/src/main/java/org/chronopolis/earth/domain/SyncStatus.java
+++ earth-driver/src/main/java/org/chronopolis/earth/domain/SyncStatus.java
@@ -5,5 +5,5 @@ package org.chronopolis.earth.domain;
  * Created by shake on 8/5/16.
  */
 public enum SyncStatus {
-    SUCCESS, FAIL_REMOTE, FAIL_LOCAL
+    SUCCESS, WARN, FAIL, FAIL_REMOTE, FAIL_LOCAL
 }
